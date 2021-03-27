f76d475fae276a303d01e2f3a2a4c9b1894a831f
andrian-kars <andrian.karsanashvili@gmail.com>
2021-03-27 20:57:20 +0200

    jest, tdd profileReducer

---
+++ src/redux/profileReducer .test.js
@@ -0,0 +1,34 @@
+import profileReducer, { addPostActionCreator, deletePost } from "./profileReducer"
+
+const state = {
+    profile: null,
+    posts: [
+        { id: 1, likesCount: 69, message: 'Oh, you\'re approuching me?' },
+        { id: 2, likesCount: 340, message: 'Dio, you are going down!' },
+        { id: 3, likesCount: 420, message: 'It was me, DIO!' }
+    ]
+}
+
+test('length of post should be increamented', () => {
+    const action = addPostActionCreator('GG!')
+
+    const newState = profileReducer(state, action)
+
+    expect(newState.posts.length).toBe(4)
+})
+
+test('checking for correct message', () => {
+    const action = addPostActionCreator('GG!')
+
+    const newState = profileReducer(state, action)
+
+    expect(newState.posts[3].message).toBe('GG!')
+})
+
+test('after deleting length of messages should be decreased', () => {
+    const action = deletePost(1)
+
+    const newState = profileReducer(state, action)
+
+    expect(newState.posts.length).toBe(2)
+})
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -3,6 +3,7 @@ import { profileAPI } from "../api/api"
 const ADD_POST = 'ADD_POST'
 const SET_USER_PROFILE = 'SET_USER_PROFILE'
 const SET_STATUS = 'SET_STATUS'
+const DELETE_POST = 'DELETE_POST'
 
 const initialState = {
     profile: null,
@@ -33,13 +34,20 @@ const profileReducer = (state = initialState, action) => {
                 status: action.status
             }
         }
+        case DELETE_POST: {
+            return {
+                ...state,
+                posts: state.posts.filter(p => p.id !== action.postId)
+            }
+        }
         default: return state
     }
 }
 
-export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile })
-export const addPostActionCreator = (newPostBody) => ({ type: ADD_POST, newPostBody })
-export const setStatus = (status) => ({ type: SET_STATUS, status })
+export const setUserProfile = profile => ({ type: SET_USER_PROFILE, profile })
+export const addPostActionCreator = newPostBody => ({ type: ADD_POST, newPostBody })
+export const setStatus = status => ({ type: SET_STATUS, status })
+export const deletePost = postId => ({ type: DELETE_POST, postId })
 
 export const getProfile = (userId) => {
     return (dispatch) => {
