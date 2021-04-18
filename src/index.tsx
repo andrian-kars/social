566158dc0bbef700daa2a83a43f9ca5d8079b6dd
andrian-kars <andrian.karsanashvili@gmail.com>
2021-04-18 18:53:26 +0300

    types x2

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import reportWebVitals from './reportWebVitals'
 import ReactDOM from 'react-dom'
 import './index.scss'
@@ -5,10 +6,14 @@ import MainApp from './App'
 
 ReactDOM.render(<MainApp />, document.getElementById('root'))
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <MainApp />,
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
-
-  // < React.StrictMode >
