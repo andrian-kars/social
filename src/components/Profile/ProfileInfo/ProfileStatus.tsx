413841f23a8c957148f9ff041db4b66e3ad267d8
andrian-kars <andrian.karsanashvili@gmail.com>
2021-04-18 16:54:41 +0300

    Types ProfileStatus

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -1,13 +1,18 @@
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import s from './ProfileInfo.module.scss'
 
-const ProfileStatusWithHooks = (props: any) => {
+type PropsType = {
+    status: string
+    updateStatus: (statusHook: string) => void
+}
+
+const ProfileStatus: React.FC<PropsType> = ({ status, updateStatus }) => {
     const [editMode, setEditMode] = useState(false)
-    const [status, setStatus] = useState(props.status)
+    const [statusHook, setStatus] = useState(status)
 
     useEffect(() => {
-        setStatus(props.status)
-    }, [props.status])
+        setStatus(status)
+    }, [status])
 
     const activateEditMode = () => {
         setEditMode(true)
@@ -15,21 +20,21 @@ const ProfileStatusWithHooks = (props: any) => {
 
     const deactivateEditMode = () => {
         setEditMode(false)
-        props.updateStatus(status)
+        updateStatus(statusHook)
     }
 
-    const onStatusChange = (e: any) => {
+    const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
         setStatus(e.currentTarget.value)
     }
 
     return (
         <>
             <p onClick={activateEditMode} className={s.status}>
-                {!editMode ? <span className={s.staticStatus}>{props.status || '-----'}</span>
-                    : <input onChange={onStatusChange} autoFocus={true} onBlur={deactivateEditMode} className={`${s.changeStatus}`} value={status} />}
+                {!editMode ? <span className={s.staticStatus}>{status || '-----'}</span>
+                    : <input onChange={onStatusChange} autoFocus={true} onBlur={deactivateEditMode} className={`${s.changeStatus}`} value={statusHook} />}
             </p>
         </>
     )
 }
 
-export default ProfileStatusWithHooks
+export default ProfileStatus
