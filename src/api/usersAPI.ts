9ce7ef6b70e5a69502ab95d59f61d51048da2ad9
andrian-kars <andrian.karsanashvili@gmail.com>
2021-04-20 18:12:05 +0300

    api select fix

--- src/api/usersAPI.ts
+++ src/api/usersAPI.ts
@@ -4,7 +4,7 @@ import { GetItemsType, instance, APIResponseType } from "./api"
 
 export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 10, term = '', friend: null | boolean = null) { 
-        return instance.get<GetItemsType>(`users?page=${currentPage}&count=${pageSize}&term=${term} + ${friend === null ? '' : `&friend=${friend}`}`)
+        return instance.get<GetItemsType>(`users?page=${currentPage}&count=${pageSize}&term=${term}${friend === null ? '' : `&friend=${friend}`}`)
             .then(response => { return response.data })
     },
     postFollow(id: number) { return instance.post<APIResponseType>(`follow/${id}/`).then(res => res.data) },
